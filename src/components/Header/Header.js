c1172605ca1cf61e75d72ee054b6a301cac5aabf
Kaja Løvsjø Solberg <kajalso@stud.ntnu.no>
2018-09-21 20:34:15 +0200

    Has created a color palette and styled the radio buttons and the other components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
 
   render() {
     const activeStyle = {
-      backgroundColor: 'rgb(181, 185, 201)'
+      backgroundColor: 'rgb(216,223,187)'
     }
     return (
       <div className="Nav">
